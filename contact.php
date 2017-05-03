36e57b8008be378123a8487d86cc72f3fd0d1a8d
danale <danale@danales-MacBook-Pro.local>
2017-05-02 20:34:47 -0400

    Developing form validation

--- contact.php
+++ contact.php
@@ -4,11 +4,72 @@
 
 	include('includes/header.php'); 
 
+	/*
+		NOTE:
+		In the form in contact.php, the name text field has
+
+		PHP used in this script:
+
+		pre_match()
+		- Perform a regular expression match
+		- http://ca2.php.net/preg_match
+
+		$_POST
+		- An associative array of variables passed to the current script via the HTTP POST method.
+		- http://www.php.net/manual/en/reserved.variables.post.php
+
+		trim()
+		- Strip whitespace (or other characters) from the beginning and end of a string
+		- http://www.php.net/manual/en/function.trim.php
+
+		exit
+		- output a message and terminate the current script
+		- http://www.php.net/manual/en/function.exit.php
+
+		die()
+		- Equivalent to exit
+		- http://ca1.php.net/manual/en/function.die.php
+
+		wordwrap()
+		- Wraps a string to a given number of characters
+		- http://ca1.php.net/manual/en/function.wordwrap.php
+
+		mail()
+		- Send mail
+		- http://ca1.php.net/manual/en/function.mail.php
+	*/
+
 ?>
 
 	<div id="contact">
 		<hr>
 		<h1 class="contact">Get in touch with us!</h1>
+
+		<?php 
+
+			// Check for header injections
+			function has_header_injection($str) {
+				return preg_match("/[\r\n]/", $str);
+			}
+
+			if (isset($_POST['contact_submit'])) {
+				$name 	= trim($_POST['name']);
+				$email 	= trim($_POST['email']);
+				$msg 	= $_POST['message'];
+
+				// Check to see if $name or $email have header injections
+				if (has_header_injection($name) || has_header_injection($email)) {
+					die(); // If true, kill the script
+				}
+
+				if (!$name || !$email || !$msg) {
+					echo '<h4 class="error">All fields required.</h4><a href="contact.php" class="button block">Go back and try again</a>';
+					exit;
+				}
+			}
+
+		?>
+
 		<form method="post" action="" id="contact-form">
 			<label for="name">Your name</label>
 			<input type="text" id="name" name="name">
